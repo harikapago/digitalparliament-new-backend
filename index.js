67228875fbe58a6ddb9e3f840a2c54197446b19d
harikapago <146166025+harikapago@users.noreply.github.com>
2023-11-01 22:28:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,24 +8,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-
-  mongoose.connect("mongodb://nodebackend-for-dpapp-server:gWvKSCn8OvkfVvYzgBqtQ4YeaNjfhppkBtLwchLs5kiGZes901NUq8v0KheDNZVJRvci1lhMnIuBACDbixqp1Q==@nodebackend-for-dpapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nodebackend-for-dpapp-server@")
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error);
-  });
-// addded azure
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to database');
-});
-
-
-
--------------------------------------registrtion-------------------------------
+// Define a Mongoose schema for the registration collection
 const registrationSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
@@ -38,6 +21,14 @@ const registrationSchema = new mongoose.Schema({
 // Create a Mongoose model based on the schema
 const Registration = mongoose.model('Registration', registrationSchema);
 
+// Connect to MongoDB
+mongoose.connect("mongodb://nodebackend-for-dpapp-server:gWvKSCn8OvkfVvYzgBqtQ4YeaNjfhppkBtLwchLs5kiGZes901NUq8v0KheDNZVJRvci1lhMnIuBACDbixqp1Q==@nodebackend-for-dpapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nodebackend-for-dpapp-server@")
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
 
 // Handle POST request to create a new registration
 app.post('/registration', async (req, res) => {
@@ -61,11 +52,9 @@ app.post('/registration', async (req, res) => {
   }
 });
 
-
 app.get('/', (req, res) => {
-    res.send('Hello, Digital Parliament world');
-  });
-
+  res.send('Hello, Digital Parliament world');
+});
 
 // Start the Express server
 app.listen(port, () => {
