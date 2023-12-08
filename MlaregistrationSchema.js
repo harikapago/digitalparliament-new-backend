ede85559ae33f7512d3e14e64dcbfd52170bccd5
harikapago <146166025+harikapago@users.noreply.github.com>
2023-12-08 11:18:29 +0530

    Create MlaregistrationSchema.js

---
+++ MlaregistrationSchema.js
@@ -0,0 +1,97 @@
+// Import necessary modules
+const mongoose = require('mongoose');
+
+// Define MLA schema
+const mlaSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+  },
+  dob: {
+    type: Date,
+   
+  },
+  gender: {
+    type: String,
+   
+  },
+  mlaPhoto:{
+    type: String,
+   
+  },
+  designation: {
+    type: String,
+   
+  },
+  presentAddress: {
+    type: String,
+   
+  },
+  permanentAddress: {
+    type: String,
+   
+  },
+  aadharCardNumber: {
+    type: Number,
+   
+    unique: true
+  },
+  aadharCardPhoto: {
+    type: String,
+   
+  },
+
+  email: {
+    type: String,
+   
+    unique: true
+  },
+  phone: {
+    type: String,
+   
+  },
+  highestEducationQualification: {
+    type: String,
+   
+  },
+  party: {
+    type: String,
+   
+  },
+  electionSymbolPath: {
+    type: String,
+   
+  },
+  nominationPapersPath: {
+    type: String,
+   
+  },
+  state: {
+    type: String,
+   
+  },
+  district: {
+    type: String,
+   
+  },
+  constituency: {
+    type: String,
+   
+  },
+  pinCodes: {
+    type: [String],
+   
+  },
+  latitude: {
+    type: String,
+   
+  },
+  longitude: {
+    type: String,
+   
+  }
+});
+
+// Create the MLA model
+const MLA = mongoose.model('MLA', mlaSchema);
+
+module.exports = MLA;
