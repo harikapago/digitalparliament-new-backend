39d57b7b537747f28b7fd7e2dfc3d657d7a9b3a9
harikapago <146166025+harikapago@users.noreply.github.com>
2023-12-19 14:54:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,32 @@ app.post('/registration', async (req, res) => {
   }
 });
 
+
+
+// Update route for changing approvalStatus to "grievance cleared"
+app.put('/user-updatestatus/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+   
+    const updatedUser = await Registration.findOneAndUpdate(
+      { _id: id },
+      { $set: { approvalStatus: 'approved' } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: 'User not found or already cleared' });
+    }
+
+    res.status(200).json({ message: 'User approval status updated to Approved', user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
 // Retrieve all registrations
 app.get('/registrations', async (req, res) => {
   try {
