e8a8b9da48f4f5717ba4f6246f28c174914d62a0
harikapago <146166025+harikapago@users.noreply.github.com>
2023-12-28 09:57:15 +0530

    Create MakePost.js

---
+++ MakePost.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  postType: {
+    type: String,
+    required: true,
+  },
+  postCategory: {
+    type: String,
+    required: true,
+  },
+  postShortDescription: {
+    type: String,
+    required: true,
+  },
+  postLongDescription: {
+    type: String,
+    required: true,
+  },
+  createdDateTime: {
+    type: Date,
+    default: Date.now,
+  },
+  mediaFilePath: {
+    type: String,
+    required: true,
+  },
+  likesCount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const MakePost = mongoose.model('MakePost', postSchema);
+
+module.exports = MakePost;
